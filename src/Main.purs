7ec2402e640c37d81ffcf13f5e02a5ce8b92b06f
hbybyyang <2451759073@qq.com>
2022-05-23 02:09:46 +0000

    .

--- src/Main.purs
+++ src/Main.purs
@@ -1,17 +1,9 @@
 module Main where
 
 import Prelude
-import Lib.DB (get_xs_by_id)
 import Effect (Effect)
-import Hby.Task (runTask_)
-import Hby.Task as T
-import Lib.Lib (initEnv, testFun)
+import Effect.Console (log)
 
 main :: Effect Unit
-main =
-  runTask_ do
-    initEnv
-    testFun
-    r <- get_xs_by_id 1
-    T.log $ show $ r
-    T.log "🍝"
+main = do
+  log "🍝"
