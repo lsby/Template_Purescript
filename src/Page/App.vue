1c3f44ff988dcf4c7a6bbbee9fd11eecde53bd81
hbybyyang <2451759073@qq.com>
2022-07-02 14:19:58 +0800

    .

--- src/Page/App.vue
+++ src/Page/App.vue
@@ -1,21 +1,15 @@
 <template>
-  <div>{{ state.hello }}!</div>
-  <div>{{ state.n }}</div>
-  <button @click="event.increase()">增加</button>
-  <button @click="event.makeZero()">归零</button>
-  <button @click="event.testElectronSync()">测试electron同步事件</button>
-  <button @click="event.testElectronAsync_on()">
-    测试electron异步事件_打开监听
-  </button>
-  <button @click="event.testElectronAsync_send()">
-    测试electron异步事件_发送
-  </button>
+  <Demo></Demo>
 </template>
 
 <script lang="ts">
 import { getCurrentInstance } from "vue"
+import Demo from "./Demo.vue"
 
 export default {
+  components: {
+    Demo,
+  },
   setup() {
     var { proxy } = getCurrentInstance() as any
     var { state, event } = proxy
--- src/Web.purs
+++ src/Web.purs
@@ -1,7 +1,6 @@
 module Web (state, event) where
 
 import Prelude
-
 import Data.Argonaut as A
 import Data.Either (Either(..))
 import Hby.Electron.IPCRenderer (on, send, sendSync)
@@ -12,6 +11,12 @@ import Lib.Vue as V
 import Type.Proxy (Proxy(..))
 
 ----------------------
+n :: Int
+n = 0
+
+hello :: String
+hello = "hello, world!"
+
 type State =
   { n :: Int
   , hello :: String
@@ -19,9 +24,29 @@ type State =
 
 state :: Task (VueReactive State)
 state = V.mk
-  { n: 0
-  , hello: "hello, world!"
+  { n: n
+  , hello
   }
+
+----------------------
+type Event =
+  { increase :: Task Unit
+  , makeZero :: Task Unit
+  , testElectronSync :: Task Unit
+  , testElectronAsync_on :: Task Unit
+  , testElectronAsync_send :: Task Unit
+  }
+
+event :: Task (VueReactive Event)
+event = do
+  s <- state
+  V.mk
+    { increase: increase s
+    , makeZero: makeZero s
+    , testElectronSync: testElectronSync
+    , testElectronAsync_on: testElectronAsync_on
+    , testElectronAsync_send: testElectronAsync_send
+    }
 
 ----------------------
 add1 :: Int -> Int
@@ -56,23 +81,3 @@ testElectronAsync_on = on "testAsync-reply"
 
 testElectronAsync_send :: Task Unit
 testElectronAsync_send = send "testAsync" $ A.encodeJson { msg: "testAsync-toService" }
-
-----------------------
-type Event =
-  { increase :: Task Unit
-  , makeZero :: Task Unit
-  , testElectronSync :: Task Unit
-  , testElectronAsync_on :: Task Unit
-  , testElectronAsync_send :: Task Unit
-  }
-
-event :: Task (VueReactive Event)
-event = do
-  s <- state
-  V.mk
-    { increase: increase s
-    , makeZero: makeZero s
-    , testElectronSync: testElectronSync
-    , testElectronAsync_on: testElectronAsync_on
-    , testElectronAsync_send: testElectronAsync_send
-    }
