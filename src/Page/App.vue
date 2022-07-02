8d2e27fe1d66113f25cd7ce032bb007d7c16bddd
hbybyyang <2451759073@qq.com>
2022-07-02 12:18:40 +0800

    electron事件测试

--- src/Page/App.vue
+++ src/Page/App.vue
@@ -3,6 +3,13 @@
   <div>{{ state.n }}</div>
   <button @click="event.increase()">增加</button>
   <button @click="event.makeZero()">归零</button>
+  <button @click="event.testElectronSync()">测试electron同步事件</button>
+  <button @click="event.testElectronAsync_on()">
+    测试electron异步事件_打开监听
+  </button>
+  <button @click="event.testElectronAsync_send()">
+    测试electron异步事件_发送
+  </button>
 </template>
 
 <script lang="ts">
--- src/Web.purs
+++ src/Web.purs
@@ -2,7 +2,11 @@ module Web (state, event) where
 
 import Prelude
 
+import Data.Argonaut as A
+import Data.Either (Either(..))
+import Hby.Electron.IPCRenderer (on, send, sendSync)
 import Hby.Task (Task)
+import Hby.Task as T
 import Lib.Vue (VueReactive)
 import Lib.Vue as V
 import Type.Proxy (Proxy(..))
@@ -31,10 +35,35 @@ makeZero :: VueReactive State -> Task Unit
 makeZero ref = do
   V.set (Proxy :: Proxy "n") 0 ref
 
+testElectronSync :: Task Unit
+testElectronSync = do
+  r <- sendSync "testSync" $ A.encodeJson { msg: "testSync-toService" }
+  case A.decodeJson r of
+    Left err -> do
+      T.log $ show err
+    Right (rx :: { msg :: String }) -> do
+      T.log $ show rx
+
+testElectronAsync_on :: Task Unit
+testElectronAsync_on = on "testAsync-reply"
+  ( \_ a -> do
+      case A.decodeJson a of
+        Left err -> do
+          T.log $ show err
+        Right (rx :: { msg :: String }) -> do
+          T.log $ show rx
+  )
+
+testElectronAsync_send :: Task Unit
+testElectronAsync_send = send "testAsync" $ A.encodeJson { msg: "testAsync-toService" }
+
 ----------------------
 type Event =
   { increase :: Task Unit
   , makeZero :: Task Unit
+  , testElectronSync :: Task Unit
+  , testElectronAsync_on :: Task Unit
+  , testElectronAsync_send :: Task Unit
   }
 
 event :: Task (VueReactive Event)
@@ -43,4 +72,7 @@ event = do
   V.mk
     { increase: increase s
     , makeZero: makeZero s
+    , testElectronSync: testElectronSync
+    , testElectronAsync_on: testElectronAsync_on
+    , testElectronAsync_send: testElectronAsync_send
     }
