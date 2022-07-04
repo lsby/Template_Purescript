f5943583951f04cd17d00720590cf5ffcef89d82
hbybyyang <2451759073@qq.com>
2022-07-04 10:10:17 +0800

    .

--- indexWeb.js
+++ indexWeb.js
@@ -1,11 +1,11 @@
 import { createApp } from "vue"
 import App from "./src/Page/App.vue"
-import api from "./output/Web/index.js"
+import Web from "./output/Web/index.js"
 
 async function main() {
   var app = createApp(App)
-  app.config.globalProperties.state = await api.state()
-  app.config.globalProperties.event = await api.event()
+  app.config.globalProperties.state = await Web.state()
+  app.config.globalProperties.event = await Web.event()
   app.mount("#app")
 }
 main()
