42f47fca9ce05a663e50bbc21ebae81c0b227bfd
hbybyyang <2451759073@qq.com>
2022-07-07 06:08:32 +0800

    .

--- src/Lib/DB.purs
+++ src/Lib/DB.purs
@@ -4,17 +4,10 @@ import Data.Maybe (Maybe(..))
 import Hby.Task (Task)
 
 ----------------------
--- | 学生id
-newtype StudentId = StudentId Int
-
--- | 学生姓名
-newtype StudentName = StudentName String
-
-----------------------
-foreign import _getNameById :: (forall a. a -> Maybe a) -> (forall a. Maybe a) -> StudentId -> Task (Maybe StudentName)
+foreign import _getNameById :: (forall a. a -> Maybe a) -> (forall a. Maybe a) -> Int -> Task (Maybe String)
 
 -- | 通过学生id获得学生姓名
-getNameById :: StudentId -> Task (Maybe StudentName)
+getNameById :: Int -> Task (Maybe String)
 getNameById id = _getNameById Just Nothing id
 
 ----------------------
