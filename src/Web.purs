f6889d020a485c24e15eb4d933675734ae720edd
hbybyyang <2451759073@qq.com>
2022-07-09 11:25:21 +0800

    更新模板

--- src/Web.purs
+++ src/Web.purs
@@ -32,48 +32,48 @@ state = V.mk
 ----------------------
 -- | 前端事件
 type Event =
-  { onClick_Increase :: Task Unit
-  , onClick_MakeZero :: Task Unit
-  , onClick_SyncSendTest :: Task Unit
-  , onClick_AsyncListener :: Task Unit
-  , onClick_AsyncSendTest :: Task Unit
-  , onInput_Todo :: ToDoItem -> Task Unit
-  , onClick_AddTodo :: Task Unit
+  { onIncrease :: Task Unit
+  , onMakeZero :: Task Unit
+  , onSyncSendTest :: Task Unit
+  , onAsyncListener :: Task Unit
+  , onAsyncSendTest :: Task Unit
+  , onUpdateTodoText :: ToDoItem -> Task Unit
+  , onAddTodo :: Task Unit
   }
 
 event :: Task (VueReactive Event)
 event = do
   s <- state
   V.mk
-    { onClick_Increase: V.apply onClick_Increase s
-    , onClick_MakeZero: V.apply onClick_MakeZero s
-    , onClick_SyncSendTest: onClick_SyncSendTest
-    , onClick_AsyncListener: onClick_AsyncListener
-    , onClick_AsyncSendTest: onClick_AsyncSendTest
-    , onInput_Todo: \a -> V.apply (onInput_Todo a) s
-    , onClick_AddTodo: V.apply onClick_AddTodo s
+    { onIncrease: V.apply onIncrease s
+    , onMakeZero: V.apply onMakeZero s
+    , onSyncSendTest: onSyncSendTest
+    , onAsyncListener: onAsyncListener
+    , onAsyncSendTest: onAsyncSendTest
+    , onUpdateTodoText: \a -> V.apply (onUpdateTodoText a) s
+    , onAddTodo: V.apply onAddTodo s
     }
 
 ----------------------
 -- | 当点击添加待办项
-onClick_AddTodo :: State -> Task State
-onClick_AddTodo s = pure $ s { toDoList = addToDoItem s.inputTodo s.toDoList, inputTodo = emptyToDoItem }
+onAddTodo :: State -> Task State
+onAddTodo s = pure $ s { toDoList = addToDoItem s.inputTodo s.toDoList, inputTodo = emptyToDoItem }
 
 -- | 当输入待办项
-onInput_Todo :: ToDoItem -> State -> Task State
-onInput_Todo str s = pure $ s { inputTodo = str }
+onUpdateTodoText :: ToDoItem -> State -> Task State
+onUpdateTodoText str s = pure $ s { inputTodo = str }
 
 -- | 当点击增加按钮
-onClick_Increase :: State -> Task State
-onClick_Increase s = pure $ s { n = addCounter 1 s.n }
+onIncrease :: State -> Task State
+onIncrease s = pure $ s { n = addCounter 1 s.n }
 
 -- | 当点击归零按钮
-onClick_MakeZero :: State -> Task State
-onClick_MakeZero s = pure $ s { n = emptyCounter }
+onMakeZero :: State -> Task State
+onMakeZero s = pure $ s { n = emptyCounter }
 
 -- | 当点击同步测试按钮
-onClick_SyncSendTest :: Task Unit
-onClick_SyncSendTest = do
+onSyncSendTest :: Task Unit
+onSyncSendTest = do
   r <- sendSync "testSync" $ A.encodeJson { msg: "testSync-toService" }
   case A.decodeJson r of
     Left err -> do
@@ -82,8 +82,8 @@ onClick_SyncSendTest = do
       T.log $ show rx
 
 -- | 当点击异步监听按钮
-onClick_AsyncListener :: Task Unit
-onClick_AsyncListener = on "testAsync-reply"
+onAsyncListener :: Task Unit
+onAsyncListener = on "testAsync-reply"
   ( \_ a -> do
       case A.decodeJson a of
         Left err -> do
@@ -93,5 +93,5 @@ onClick_AsyncListener = on "testAsync-reply"
   )
 
 -- | 当点击异步测试按钮
-onClick_AsyncSendTest :: Task Unit
-onClick_AsyncSendTest = send "testAsync" $ A.encodeJson { msg: "testAsync-toService" }
+onAsyncSendTest :: Task Unit
+onAsyncSendTest = send "testAsync" $ A.encodeJson { msg: "testAsync-toService" }
