4e2820e87ede33dc65737040182e41f71fe093d3
hbybyyang <2451759073@qq.com>
2022-07-10 13:05:40 +0800

    .

--- src/Tools/GenTypes.purs
+++ src/Tools/GenTypes.purs
@@ -14,8 +14,6 @@ import Type.Proxy (Proxy(..))
 import Web (State, Event)
 
 -- | 生成前端状态和事件的TS类型描述
--- | 注意: 不要生成泛型类型, 在TS里无法完整表达完整的类型约束, 应当将其包裹为特化类型.
--- | 注意: 尽可能生成Pure类型, 使TS无法操作, 以保证安全.
 genTypes :: Effect Unit
 genTypes = do
   p <- PATH.resolve [ __dirname ] "../../src/Page/Types.ts"
